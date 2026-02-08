5f8b2c10c6d8487ae6cc853da94e9231cfe91963
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2026-02-08 18:21:31 +0100

    Modification dans la configuration de next.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://backend:3000/:path*',
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
