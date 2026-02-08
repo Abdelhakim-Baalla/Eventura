5e5e8d79b5521215182719b0cf0f12db0762a561
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2026-02-08 16:33:52 +0100

    Creation de Authentification service

---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,47 @@
+import { Injectable, ConflictException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { User } from '../users/entities/user.entity';
+import { RegisterDto } from './dto/register.dto';
+import { Role } from '../common/enums/role.enum';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async register(
+    registerDto: RegisterDto,
+  ): Promise<{ message: string; user: Partial<User> }> {
+    const { email, password, nom, prenom, telephone } = registerDto;
+
+    const existingUser = await this.userRepository.findOne({
+      where: { email },
+    });
+    if (existingUser) {
+      throw new ConflictException('Cet email est déjà utilisé');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = this.userRepository.create({
+      email,
+      password: hashedPassword,
+      nom,
+      prenom,
+      telephone,
+      role: Role.PARTICIPANT,
+    });
+
+    await this.userRepository.save(user);
+
+    const { password: _, ...userWithoutPassword } = user;
+    return {
+      message: 'Inscription réussie',
+      user: userWithoutPassword,
+    };
+  }
+}
