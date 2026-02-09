738875313e986bb1166daf2ad935258e857c4d01
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2026-02-09 20:19:32 +0100

    Creation de controlleur des events

---
+++ backend/src/events/events.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Post, Put, Body, Param, Request } from '@nestjs/common';
+import { EventsService } from './events.service';
+import { CreateEventDto } from './dto/create-event.dto';
+import { Roles } from '../common/decorators/roles.decorator';
+import { Role } from '../common/enums/role.enum';
+
+@Controller('events')
+export class EventsController {
+  constructor(private readonly eventsService: EventsService) {}
+
+  @Roles(Role.ADMIN, Role.SUPER_ADMIN)
+  @Post()
+  async create(@Body() createEventDto: CreateEventDto, @Request() req) {
+    const createurId = req.user.sub;
+    return this.eventsService.create(createEventDto, createurId);
+  }
+
+  @Roles(Role.ADMIN, Role.SUPER_ADMIN)
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateEventDto: Partial<CreateEventDto>,
+  ) {
+    return this.eventsService.update(id, updateEventDto);
+  }
+}
