67c51cb7b6b47655a46a54596de946b9b6da4b92
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2026-02-10 10:47:20 +0100

    feat: Implement event creation and editing pages with rich text editor, preview functionality, and necessary dependencies.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -9,6 +9,7 @@ import { Category } from './users/entities/category.entity';
 import { Event } from './users/entities/event.entity';
 import { Reservation } from './users/entities/reservation.entity';
 import { AuthModule } from './auth/auth.module';
+import { EventsModule } from './events/events.module';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { RolesGuard } from './auth/guards/roles.guard';
 
@@ -30,6 +31,7 @@ import { RolesGuard } from './auth/guards/roles.guard';
       logging: true,
     }),
     AuthModule,
+    EventsModule,
   ],
   controllers: [AppController],
   providers: [
